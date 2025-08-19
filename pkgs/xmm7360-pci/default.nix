385790dc8c1aac5595a94e57fcba9e19a0608cb1
Cedric Reitz <cedricreitz@outlook.com>
2025-08-19 15:09:59 +0200

    c

--- pkgs/xmm7360-pci/default.nix
+++ pkgs/xmm7360-pci/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "xmm7360-pci";
-  version = "unstable-2024-01-15";
+  version = "unstable-2025-81-19";
 
   src = fetchFromGitHub {
     owner = "xmm7360";
@@ -10,12 +10,20 @@ stdenv.mkDerivation rec {
     rev = "master";
     sha256 = "sha256-wwm9ELALiJrC54azyJ95Rm3pcGLYzhxEe9mcCUvSVKk=";
   };
-
-  # Add the patch for kernel 6.12 compatibility
-  patches = [ ./kernel-6.12-compat.patch ];
-
   nativeBuildInputs = [ kmod ];
   buildInputs = [ kernel.dev ];
+
+  postPatch = ''
+    # Fix tty_write function signature for kernel 6.12+
+    sed -i 's/static int xmm7360_tty_write(struct tty_struct \*tty, const unsigned char \*buffer, int count)/static ssize_t xmm7360_tty_write(struct tty_struct *tty, const u8 *buffer, size_t count)/' xmm7360.c
+    
+    # Fix return type casts
+    sed -i 's/return count;/return (ssize_t)count;/g' xmm7360.c
+    sed -i 's/return xmm7360_qp_write/return (ssize_t)xmm7360_qp_write/' xmm7360.c
+    
+    # Fix count comparison
+    sed -i 's/if (ret > count)/if (ret > (int)count)/' xmm7360.c
+  '';
 
   makeFlags = [
     "KDIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
